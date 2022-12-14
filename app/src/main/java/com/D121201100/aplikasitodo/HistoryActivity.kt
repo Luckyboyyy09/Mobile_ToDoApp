95630b7f055ea4988fab205782f244ff62468e61
Luckyboyyy09 <muhthoriq0111@gmail.com>
2022-12-14 15:29:14 +0800

    Membuat ToDoApp

---
+++ app/src/main/java/com/D121201100/aplikasitodo/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.D121201100.aplikasitodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
