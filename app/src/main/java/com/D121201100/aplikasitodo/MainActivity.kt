e785588a364a155c92be66917084a654b790239e
Luckyboyyy09 <muhthoriq0111@gmail.com>
2022-12-14 16:52:13 +0800

    tampilan baru pada layout nya

--- app/src/main/java/com/D121201100/aplikasitodo/MainActivity.kt
+++ app/src/main/java/com/D121201100/aplikasitodo/MainActivity.kt
@@ -7,7 +7,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
+//import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -150,39 +150,6 @@ class MainActivity : AppCompatActivity() {
         itemTouchHelper.attachToRecyclerView(todoRv)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        val item = menu.findItem(R.id.search)
-        val searchView = item.actionView as SearchView
-        item.setOnActionExpandListener(object :MenuItem.OnActionExpandListener{
-            override fun onMenuItemActionExpand(item: MenuItem): Boolean {
-                displayTodo()
-                return true
-            }
-
-            override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
-                displayTodo()
-                return true
-            }
-
-        })
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                if(!newText.isNullOrEmpty()){
-                    displayTodo(newText)
-                }
-                return true
-            }
-
-        })
-
-        return super.onCreateOptionsMenu(menu)
-    }
-
     fun displayTodo(newText: String = "") {
         db.todoDao().getTask().observe(this, Observer {
             if(it.isNotEmpty()){
@@ -195,15 +162,6 @@ class MainActivity : AppCompatActivity() {
                 adapter.notifyDataSetChanged()
             }
         })
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.history -> {
-                startActivity(Intent(this, HistoryActivity::class.java))
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     fun openNewTask(view: View) {
